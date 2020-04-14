e842a7183b9ba1a591515721e5c839101396d38a
Michelle Morin <morin2m@gmail.com>
2020-04-14 15:07:53 -0700

    fix typo and adjust ranges
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- src/standalone.js
+++ src/standalone.js
@@ -1,25 +1,25 @@
 import $ from 'jquery';
 
 export function updateStatus(plant) {
-  if(plant.food < 3) {
+  if(plant.soil < 5) {
     $("#foodLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
-  } else if(plant.soil <= 6) {
+  } else if(plant.soil <= 15) {
     $("#foodLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
-  } else if(plant.soil > 6) {
+  } else if(plant.soil > 15) {
     $("#foodLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
   }
-  if(plant.water < 3) {
+  if(plant.water < 10) {
     $("#waterLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
-  } else if(plant.water <= 6) {
+  } else if(plant.water <= 20) {
     $("#waterLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
-  } else if(plant.water > 6) {
+  } else if(plant.water > 20) {
     $("#waterLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
   }
-  if(plant.light < 3) {
+  if(plant.light < 10) {
     $("#lightLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
-  } else if(plant.light <= 6) {
+  } else if(plant.light <= 20) {
     $("#lightLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
-  } else if(plant.light > 6) {
+  } else if(plant.light > 20) {
     $("#lightLevel1 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
   }
   $("#foodLevel1 .progress-bar").attr('style', `width: ${plant.soil*10}%`);
@@ -28,25 +28,25 @@ export function updateStatus(plant) {
 }
 
 export function updateStatus2(plant) {
-  if(plant.food < 3) {
+  if(plant.soil < 5) {
     $("#foodLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
-  } else if(plant.soil <= 6) {
+  } else if(plant.soil <= 15) {
     $("#foodLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
-  } else if(plant.soil > 6) {
+  } else if(plant.soil > 15) {
     $("#foodLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
   }
-  if(plant.water < 3) {
+  if(plant.water < 10) {
     $("#waterLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
-  } else if(plant.water <= 6) {
+  } else if(plant.water <= 20) {
     $("#waterLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
-  } else if(plant.water > 6) {
+  } else if(plant.water > 20) {
     $("#waterLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
   }
-  if(plant.light < 3) {
+  if(plant.light < 10) {
     $("#lightLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
-  } else if(plant.light <= 6) {
+  } else if(plant.light <= 20) {
     $("#lightLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
-  } else if(plant.light > 6) {
+  } else if(plant.light > 20) {
     $("#lightLevel2 .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
   }
   $("#foodLevel2 .progress-bar").attr('style', `width: ${plant.soil*10}%`);
